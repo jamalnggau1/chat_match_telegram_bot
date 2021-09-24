38e03b8238c21ed0f4040d9cbf032f09e195925d
jamalnggau1 <44499007+jamalnggau1@users.noreply.github.com>
2021-09-25 00:08:09 +0800

    Update config.py

--- bot_poc/config.py
+++ bot_poc/config.py
@@ -1,11 +1,11 @@
 # coding: utf-8
-# 設置 App.config
+# App.config
 SECRET_KEY              = 'b07481bjava7704sd25sd27d53000e6b1daI52f47'
 SESSION_PROTECTION      = 'strong'
 JSON_AS_ASCII           = False
 #CSRF_ENABLED           = True
 TEMPLATES_AUTO_RELOAD   = True
 
-# 設置 bot_poc.config
+# bot_poc.config
 BOT_API_KEY             = "2031288797:AAHAmpVLJoLjnJ-X2VxHppbOTf_zfHmgpt0"
-BOT_HOOK_URL            = "redis-11317.c259.us-central1-2.gce.cloud.redislabs.com:11317"
+BOT_HOOK_URL            = "redis-11317.c259.us-central1-2.gce.cloud.redislabs.com"
